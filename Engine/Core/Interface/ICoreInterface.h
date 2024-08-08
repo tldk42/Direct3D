9e55484f29b8c83b2a054fb98a82e8469eea9a0f
tldk42 <tldk47@hanmail.net>
2024-08-08 23:52:32 +0900

    Interface 분리

---
+++ Engine/Core/Interface/ICoreInterface.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class ICoreInterface
+{
+public:
+	virtual ~ICoreInterface() = default;
+
+public:
+#pragma region Core Interface
+	virtual void Initialize() = 0;
+	virtual void Update(float_t DeltaTime) = 0;
+	virtual void Release() = 0;
+#pragma endregion
+};
---
+++ Engine/Core/Interface/IRenderable.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+enum class ERenderingLayer : uint8_t
+{
+	GameObject,
+	UI,
+	GUI,
+	Background_2D,
+	Foreground_2D,
+	Player_2D,
+	Particle_2D,
+};
+
+class IRenderable
+{
+public:
+	virtual void PreRender() = 0;
+	virtual void Render() = 0;
+	virtual void PostRender() = 0;
+
+	virtual ERenderingLayer GetLayerType() = 0;
+};
